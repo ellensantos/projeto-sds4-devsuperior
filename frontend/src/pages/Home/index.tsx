8d0439fc19d8a6de160bf07888ed47b99ef449bb
Ellen Teixeira Santos <ellentsantos@hotmail.com>
2021-09-12 17:07:30 -0300

    Change home page text

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -9,9 +9,9 @@ const Home = () => {
             <div className="container">
                 <div className="jumbotron">
                     <h1 className="display-4">DSVendas</h1>
-                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <p className="lead">Analisando o desempenho das vendas por diferentes perspectivas</p>
                     <hr />
-                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back-end construído com Spring Boot.</p>
                     <Link className="btn btn-warning btn-lg" to="dashboard">
                         Acessar dashboard
                     </Link>
